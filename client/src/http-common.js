c33b3b67b19dfee29b37068bcedc12bfc4af479d
miro00 <ozvard765@gmail.com>
2021-06-04 13:04:03 +0500

    mc

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -4,7 +4,8 @@ axios.defaults.withCredentials = true
 
 export default axios.create({
   // baseURL: "http://192.168.1.153:3001/api",
-  baseURL: "http://localhost:3001/api",
+  // baseURL: "http://localhost:3001/api",
+  baseURL: "http://api.helpdesk/api",
   headers: {
     "Content-type": "application/json"
   }
